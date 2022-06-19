b952f8880943e6b7f1b1a0e8120fb515eae9cb93
Naufal Anbial Falah <75089762+nafnaufal@users.noreply.github.com>
2022-06-19 22:23:15 +0700

    update index

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 $con = mysqli_connect("localhost", "root", "", "w_list");
 
-if (isset($_GET["search"])) {
-    $title = "%".$_GET["searchtitle"]."%";
-    $getList = "SELECT * FROM list WHERE title LIKE '$title'";
+if (isset($_GET["search"]) and $_GET["searchtitle"] !== "") {
+    $title = $_GET["searchtitle"];
+    $getList = "SELECT * FROM list WHERE `date` = '$title'";
     $list = mysqli_query($con, $getList);
-}else {
+} else {
     $getList = "SELECT * FROM list";
     $list = mysqli_query($con, $getList);
 }
@@ -24,31 +24,28 @@ if (isset($_GET["search"])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
     <link rel="stylesheet" href="./style.css">
-    <title>Watching list</title>
+    <title>TODO list</title>
 </head>
 
 <body>
+    <form action="./index.php" method="get">
+        <div class="hstack gap-5 m-5">
+            <h3 class="header-title">TODO List</h3>
 
-    <div class="container d-flex justify-content-center my-4">
-        <h3 class="title">
-            Watching List
-        </h3>
-    </div>
-
-    <form class="form-control" action="./index.php" method="get">
-        <input class="form-control form-control-sm" type="text" placeholder="Search title" name="searchtitle">
-        <button class="btn btn-default" type="submit" name="search">
-            <i class="bi bi-search"></i>
-        </button>
+            <div class="hstack gap-2 ms-auto">
+                <input class="form-control form-control-sm" type="date" placeholder="Search title" name="searchtitle">
+                <button class="btn btn-default btn-search" type="submit" name="search"><i class="bi bi-funnel"></i></button>
+            </div>
+        </div>
     </form>
 
-    <div class="container">
-        <table class="table table-hover">
-            <thead>
+    <div class="container-fluid px-5">
+        <table class="table table-hover align-middle">
+            <thead class="table-warning">
                 <tr>
-                    <th class="table-warning" scope="col">Time/Date</th>
-                    <th class="table-warning" scope="col">Title</th>
-                    <th class="table-warning" scope="col"></th>
+                    <th class="p-3" scope="col col-3">Time and Date</th>
+                    <th class="p-3" scope="col">Title</th>
+                    <th class="p-3" scope="col col-1"></th>
                 </tr>
             </thead>
 
@@ -63,15 +60,9 @@ if (isset($_GET["search"])) {
                 ?>
 
                             <tr onclick="window.location='./detail.php/?id=<?= $id ?>';">
-
-                                <th scope="row"><?= $row["time"] ?></br><?= $row["date"] ?></th>
-                                <td><?= $row["title"] ?></td>
-
-                                <td>
-                                    <a href="./proccess.php/?id_del=<?= $id ?>">
-                                        <i class="bi bi-trash"></i>
-                                    </a>
-                                </td>
+                                <th class="p-3 col col-2 time-table" scope="row"><?= $row["time"] ?><div class="vr mx-2"></div><?= $row["date"] ?></th>
+                                <td class="p-3"><?= $row["title"] ?></td>
+                                <td class="p-3 col-1 text-center"><a href="./proccess.php/?id_del=<?= $id ?>"><i class="bi bi-trash"></i></a></td>
                             </tr>
 
                 <?php
@@ -83,11 +74,11 @@ if (isset($_GET["search"])) {
         </table>
     </div>
 
-    <div class="container">
+    <div class="container btn-sticky">
         <div class="row justify-content-center">
-            <div class="col-1 ">
+            <div class="col-1">
                 <a href="#add" data-bs-toggle="modal" data-bs-target="#add">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="48px" fill="currentColor" class="bi bi-plus btn-sticky float-btn h-auto icon-flipped" viewBox="0 0 16 16">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="48px" fill="currentColor" class="bi bi-plus float-btn h-auto icon-flipped" viewBox="0 0 16 16">
                         <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
                     </svg>
                 </a>
@@ -99,30 +90,30 @@ if (isset($_GET["search"])) {
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="AddList">Add new watching list</h5>
+                    <h5 class="modal-title mx-2" id="AddList">Add TODO</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <form action="./proccess.php" method="post">
                     <div class="modal-body">
-                        <div class="mb-3">
-                            <label for="date" class="col-form-label">Date:</label>
+                        <div class="mb-3 mx-2">
+                            <label for="date" class="col-form-label">Date</label>
                             <input type="date" class="form-control" id="date" name="date" required>
                         </div>
-                        <div class="mb-3">
-                            <label for="time" class="col-form-label">Time:</label>
+                        <div class="mb-3 mx-2">
+                            <label for="time" class="col-form-label">Time</label>
                             <input type="time" class="form-control" id="time" name="time" required>
                         </div>
-                        <div class="mb-3">
-                            <label for="title" class="col-form-label">Title:</label>
+                        <div class="mb-3 mx-2">
+                            <label for="title" class="col-form-label">Title</label>
                             <input type="text" class="form-control" id="title" name="title" required>
                         </div>
-                        <div class="mb-3">
-                            <label for="note" class="col-form-label">Note:</label>
+                        <div class="mb-3 mx-2">
+                            <label for="note" class="col-form-label">Note</label>
                             <textarea class="form-control" id="note" name="note"></textarea>
                         </div>
                     </div>
                     <div class="modal-footer">
-                        <input type="submit" class="btn btn-primary" value="Save" name="submit"></button>
+                        <input type="submit" class="btn btn-warning" value="Save" name="submit"></button>
                     </div>
                 </form>
             </div>
