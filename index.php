a84395d860653762efe9c131de46586723ad0f1e
Yulia Dwi Putri <83277030+yuliadwiputri@users.noreply.github.com>
2022-06-19 22:20:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <?php
-// $con = mysqli_connect("localhost", "root", "", "unila");
-// $qin =  "INSERT INTO ilkom VALUES ('$_POST[id]', '$_POST[nama]', '$_POST[nosepatu]', '$_POST[tinggi]')";
-// mysqli_query($con, $qin);
+$con = mysqli_connect("localhost", "root", "", "w_list");
 
-// echo "Data Berhasil diinput!";
+if (isset($_GET["search"])) {
+    $title = "%".$_GET["searchtitle"]."%";
+    $getList = "SELECT * FROM list WHERE title LIKE '$title'";
+    $list = mysqli_query($con, $getList);
+}else {
+    $getList = "SELECT * FROM list";
+    $list = mysqli_query($con, $getList);
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -16,100 +22,73 @@
 
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
     <link rel="stylesheet" href="./style.css">
     <title>Watching list</title>
 </head>
 
 <body>
 
-    <div class="container d-flex justify-content-left my-4">
+    <div class="container d-flex justify-content-center my-4">
         <h3 class="title">
             Watching List
         </h3>
     </div>
+
+    <form class="form-control" action="./index.php" method="get">
+        <input class="form-control form-control-sm" type="text" placeholder="Search title" name="searchtitle">
+        <button class="btn btn-default" type="submit" name="search">
+            <i class="bi bi-search"></i>
+        </button>
+    </form>
+
     <div class="container">
-        <table class="table">
+        <table class="table table-hover">
             <thead>
                 <tr>
-                    <th scope="col">Time</th>
-                    <th scope="col">Title</th>
-                    <th scope="col"></th>
-                    <th scope="col"></th>
+                    <th class="table-warning" scope="col">Time/Date</th>
+                    <th class="table-warning" scope="col">Title</th>
+                    <th class="table-warning" scope="col"></th>
                 </tr>
             </thead>
+
             <tbody>
-                <tr>
-                    <th scope="row">10:10/20-02-2020</th>
-                    <td>ayam berkokok di pagi hari asdjaskljk asd aklsdja asdjaksjdjlad</td>
-                    <td>
-                        <a href="https://google.com">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
-                                <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z" />
-                            </svg>
-                        </a>
-                    </td>
-                    <td>
-                        <a href="https://google.com">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
-                                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-                            </svg>
-                        </a>
-                    </td>
-                </tr>
-                <tr>
-                    <th scope="row">10:10/20-02-2020</th>
-                    <td>Jacob</td>
-                    <td>
-                        <a href="https://google.com">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
-                                <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z" />
-                            </svg>
-                        </a>
-                    </td>
-                    <td>
-                        <a href="https://google.com">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
-                                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-                            </svg>
-                        </a>
-                    </td>
-                </tr>
-                <tr>
-                    <th scope="row">10:10/20-02-2020</th>
-                    <td>Lar ry the Bird</td>
-                    <td>
-                        <a href="https://google.com">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
-                                <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z" />
-                            </svg>
-                        </a>
-                    </td>
-                    <td>
-                        <a href="https://google.com">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
-                                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-                            </svg>
-                        </a>
-                    </td>
-                </tr>
+
+                <?php
+
+                if ($list) {
+                    if (mysqli_num_rows($list) > 0) {
+                        while ($row = mysqli_fetch_assoc($list)) {
+                            $id = $row["id"];
+                ?>
+
+                            <tr onclick="window.location='./detail.php/?id=<?= $id ?>';">
+
+                                <th scope="row"><?= $row["time"] ?></br><?= $row["date"] ?></th>
+                                <td><?= $row["title"] ?></td>
+
+                                <td>
+                                    <a href="./proccess.php/?id_del=<?= $id ?>">
+                                        <i class="bi bi-trash"></i>
+                                    </a>
+                                </td>
+                            </tr>
+
+                <?php
+                        }
+                    }
+                }
+                ?>
             </tbody>
         </table>
     </div>
 
     <div class="container">
-        <div class="row justify-content-center444">
+        <div class="row justify-content-center">
             <div class="col-1 ">
                 <a href="#add" data-bs-toggle="modal" data-bs-target="#add">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="48px" fill="currentColor" class="bi bi-plus btn-sticky float-btn h-auto" viewBox="0 0 16 16">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="48px" fill="currentColor" class="bi bi-plus btn-sticky float-btn h-auto icon-flipped" viewBox="0 0 16 16">
                         <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
-                    </svg>
-                </a>
-            </div>
-
-            <div class="col-1">
-                <a href="https://youtube.com">
-                    <svg viewBox="-7 -7 30 30" xmlns="http://www.w3.org/2000/svg" width="48px" fill="currentColor" class="bi bi-trash3 btn-sticky float-btn h-auto" viewBox="0 0 16 16">
-                        <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
                     </svg>
                 </a>
             </div>
@@ -123,29 +102,32 @@
                     <h5 class="modal-title" id="AddList">Add new watching list</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <form action="./index.php" method="post">
-                <div class="modal-body">
+                <form action="./proccess.php" method="post">
+                    <div class="modal-body">
                         <div class="mb-3">
                             <label for="date" class="col-form-label">Date:</label>
-                            <input type="date" class="form-control" id="date">
+                            <input type="date" class="form-control" id="date" name="date" required>
                         </div>
                         <div class="mb-3">
                             <label for="time" class="col-form-label">Time:</label>
-                            <input type="time" class="form-control" id="time">
+                            <input type="time" class="form-control" id="time" name="time" required>
                         </div>
                         <div class="mb-3">
                             <label for="title" class="col-form-label">Title:</label>
-                            <textarea class="form-control" id="title"></textarea>
+                            <input type="text" class="form-control" id="title" name="title" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="note" class="col-form-label">Note:</label>
+                            <textarea class="form-control" id="note" name="note"></textarea>
                         </div>
                     </div>
                     <div class="modal-footer">
-                        <input type="submit" class="btn btn-primary" value="Save"></button>
+                        <input type="submit" class="btn btn-primary" value="Save" name="submit"></button>
                     </div>
                 </form>
             </div>
         </div>
     </div>
-
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </body>
